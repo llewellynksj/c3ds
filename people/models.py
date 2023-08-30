a8aea6b349653af1d24b66f3ad1344077be4909d
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-08-30 09:42:36 +0000

    Add category choices to profile model and amend people.html

--- people/models.py
+++ people/models.py
@@ -3,8 +3,15 @@ from cloudinary.models import CloudinaryField
 
 
 class Profile(models.Model):
+    CATEGORY = [
+        ('Centre Director', 'Centre Director'),
+        ('Research Fellow', 'Research Fellow'),
+        ('Visiting Fellow', 'Visiting Fellow'),
+    ]
+
     profile_pic = CloudinaryField('image', default='profile_pic_placeholder')
     name = models.CharField(max_length=100)
+    job_category = models.CharField(max_length=100, choices=CATEGORY, default='Centre Director')
     job_title = models.CharField(max_length=500)
     discipline = models.CharField(max_length=100)
     summary = models.TextField(max_length=2000)

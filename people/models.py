aa3972173bab057831b1ee017315ecc880983baa
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-09-13 08:02:44 +0000

    Add web link to profile model and update template

--- people/models.py
+++ people/models.py
@@ -14,6 +14,7 @@ class Profile(models.Model):
     job_category = models.CharField(max_length=100, choices=CATEGORY, default='Centre Director')
     job_title = models.CharField(max_length=500)
     discipline = models.CharField(max_length=100)
+    web_link = models.TextField(max_length=2000, default='www.google.com', help_text="Please enter full web address including 'https://'")
     summary = models.TextField(max_length=2000)
 
     def __str__(self):

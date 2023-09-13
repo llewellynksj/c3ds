ad14d6d6315c8f999c60a17640ebd183d21659cf
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-09-13 08:04:09 +0000

    Tidy up people models file

--- people/models.py
+++ people/models.py
@@ -9,13 +9,25 @@ class Profile(models.Model):
         ('Visiting Fellow', 'Visiting Fellow'),
     ]
 
-    profile_pic = CloudinaryField('image', default='profile_pic_placeholder')
-    name = models.CharField(max_length=100)
-    job_category = models.CharField(max_length=100, choices=CATEGORY, default='Centre Director')
-    job_title = models.CharField(max_length=500)
-    discipline = models.CharField(max_length=100)
-    web_link = models.TextField(max_length=2000, default='www.google.com', help_text="Please enter full web address including 'https://'")
-    summary = models.TextField(max_length=2000)
+    profile_pic = CloudinaryField(
+        'image',
+        default='profile_pic_placeholder')
+    name = models.CharField(
+        max_length=100)
+    job_category = models.CharField(
+        max_length=100,
+        choices=CATEGORY,
+        default='Centre Director')
+    job_title = models.CharField(
+        max_length=500)
+    discipline = models.CharField(
+        max_length=100)
+    web_link = models.TextField(
+        max_length=2000,
+        default='www.google.com',
+        help_text="Please enter full web address including 'https://'")
+    summary = models.TextField(
+        max_length=2000)
 
     def __str__(self):
         return self.name

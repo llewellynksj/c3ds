d890796948bd1d3b0b6585e6d06e08d4313be469
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-09-25 10:19:05 +0000

    Add publications model

--- research/admin.py
+++ research/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Publication
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Publication)
+class PublicationAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description')
--- research/models.py
+++ research/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Publication(models.Model):
+    """
+    Model to hold database of information for publications
+    """
+    title = models.TextField(max_length=2000)
+    thumbnail = CloudinaryField('image', default='publication_placeholder')
+    date = models.DateField(help_text='Please enter MM/DD/YYYY')
+    type = models.CharField(max_length=255)
+    publication = models.CharField(max_length=255)
+    authors = models.TextField(max_length=2000)
+    description = models.TextField(help_text='For consistency please use Arial font, size 12, line height 1.4')
+    url = models.URLField()
+
+    class Meta:
+        """
+        Metadata Class Container
+        """
+        ordering = ['-date']
+
+    def __str__(self):
+        """
+        Function that returns main publication objects as a string
+        """
+        return f'{self.publication} - "{self.title}"'

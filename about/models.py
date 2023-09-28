8ef2d8a37ca98053af6041bd907d019608a32945
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-09-28 11:39:05 +0000

    Add basic structure of contact form to contact page

--- about/models.py
+++ about/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import EmailValidator
 
-# Create your models here.
+
+class Enquiry(models.Model):
+    """
+    Enquiry model saves contact form submissions
+    """
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(validators=[EmailValidator])
+    message = models.TextField(max_length=800)
+
+    def __str__(self):
+        return self.full_name
--- c3ds/settings.py
+++ c3ds/settings.py
@@ -148,3 +148,11 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Send Mail
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp-mail.outlook.com'
+# EMAIL_HOST_USER = os.environ.get('SEND_MAIL_EMAIL')
+# EMAIL_HOST_PASSWORD = os.environ.get('SEND_MAIL_PASSWORD')

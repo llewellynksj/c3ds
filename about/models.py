eb65ae699ce96a90f55d84686907c9410b8dbf0c
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-09-28 12:50:45 +0000

    Add metadata

--- about/models.py
+++ about/models.py
@@ -1,14 +1,1 @@
 from django.db import models
-from django.core.validators import EmailValidator
-
-
-class Enquiry(models.Model):
-    """
-    Enquiry model saves contact form submissions
-    """
-    full_name = models.CharField(max_length=100)
-    email = models.EmailField(validators=[EmailValidator])
-    message = models.TextField(max_length=800)
-
-    def __str__(self):
-        return self.full_name
--- about/views.py
+++ about/views.py
@@ -1,39 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Enquiry
-from django.contrib import messages
-from .forms import EnquiryForm
-# from django.core.mail import send_mail
-# import os
-# if os.path.isfile('env.py'):
-#     import env
 
 
 def display_contact(request):
     """
-    Displays the Contact page
-    Validates contact form and saves
-    Sends email of contact form
+    Displays the contact page
     """
-    # SEND_MAIL_EMAIL = os.environ.get('SEND_MAIL_EMAIL')
-
-    if request.method == "POST":
-        form = EnquiryForm(request.POST)
-        if form.is_valid():
-            print('form was valid')
-            form.save()
-            messages.success(request, ('Your message has been sent!'))
-
-            # Send enquiry form as email
-            # send_mail(
-            #     'C3DS Website Enquiry from ' + form.cleaned_data['full_name'],
-            #     form.cleaned_data['message'],
-            #     form.cleaned_data['email'],
-            #     [SEND_MAIL_EMAIL],
-            # )
-
-    form = EnquiryForm()
-    return render(
-        request, 'contact.html', {'form': form})
+    return render(request, 'contact.html', {})
 
 
 def display_about(request):
